324e20eb0cbe73e066fcc6414ab73dce172c8bbb
PetteriRauhala <122267930+PetteriRauhala@users.noreply.github.com>
2023-01-30 11:10:41 +0200

    Muutoksia

--- harjoitus5.py
+++ harjoitus5.py
@@ -4,21 +4,30 @@ class Auto:
         self.merkki = merkki
         self.malli = malli
         self.rekisterinumero = rekisterinumero
+        self.vuosi = None
 
     def __str__ (self) -> str:
-        # Merkkijonoja voi yhdistää plus-merkillä
-        teksti =   "Auto: " + self.merkki + " " + self.malli + " (" + self.rekisterinumero + ") "
+        # Muotoillaan Auto-olio stringiksi
 
-        # Sama f-stringillä
-        teksti = f"Auto: {self.merkki} {self.malli} ({self.rekisterinumero})"
+        # Muodostetaan ensin "vuosi"-muuttujaan -95  -tyylinen stringi
         
-        return teksti
+        #  Huom   Terniärioperaattori
+        #          A if EHTo else B
+        #        Valitsee arvon A jos EHTO on tosi, muuten arvon B
+
+        vuosi = f" -{self.vuosi % 100}" if self.vuosi is not None else ""
+        return f"Auto: {self.merkki} {self.malli}{vuosi} ({self.rekisterinumero})"
 
 
-auto1 = Auto("VW", "Kupla", "AKU-313")
-auto2 = Auto("Opel", "Kadet", "ABC-123")
-auto3 = Auto("Volvo", "240", "CBA-321")
+vw = Auto("VW", "Kupla", "AKU-313")
+opel = Auto("Opel", "Kadet", "ABC-123")
+volvo = Auto("Volvo", "240", "CBA-321")
 
-print(auto1)
-print(auto2)
-print(auto3)
+# Atribuutteja voi asettaa myös luokan ulkopuolella
+vw.vuosi = 1992
+opel.vuosi = 1982
+volvo.vuosi = 1995
+
+print(vw)
+print(opel)
+print(volvo)
